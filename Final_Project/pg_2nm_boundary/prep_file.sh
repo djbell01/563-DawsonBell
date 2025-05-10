7d56eac00c4205755ed95a6a8c6668dab9a8c840
djbell01 <131902078+djbell01@users.noreply.github.com>
2025-05-09 23:52:07 -0600

    Upload all files

---
+++ Final_Project/pg_2nm_boundary/prep_file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p short #gpu-p100, shortgpu, shortgpu-p100, short, bsudfq, 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH -t 30:00
+#SBATCH -o prep.o%j
+#SBATCH -J PREP
+#  #SBATCH --exclusive
+
+module unload gcc
+module load gromacs
+export OMP_NUM_THREADS=48
+export GMXLIB=$HOME/GMXLIB
+echo 15 | gmx_mpi pdb2gmx -f pg.pdb -o pg.gro -water tip4p
+gmx_mpi editconf -f pg.gro -o box.gro -c -d 2.0 -bt cubic
+gmx_mpi solvate -cp box.gro -cs tip4p -o conf.gro -p topol.top
---
+++ Final_Project/pg_2x_box_size/md.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -p bsudfq #gpu-p100, shortgpu, shortgpu-p100, short, bsudfq, 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH -t 24:00:00
+#SBATCH -o log_slurm.o%j
+#SBATCH -J PG2X
+#  #SBATCH --exclusive
+
+module unload gcc
+module load gromacs
+export OMP_NUM_THREADS=48
+export GMXLIB=$HOME/GMXLIB
+
+mpirun -np 1 -npernode 48 gmx_mpi grompp -f mdp/min.mdp -c reordered.gro -p topol.top -o min.tpr
+mpirun -np 1 -npernode 48 gmx_mpi mdrun -v -deffnm min
+
+mpirun -np 1 -npernode 48 gmx_mpi grompp -f mdp/min2.mdp -c min.gro -r min.gro -p topol.top -o min2.tpr
+mpirun -np 1 -npernode 48 gmx_mpi mdrun -v -deffnm min2
+
+mpirun -np 1 -npernode 48 gmx_mpi grompp -f mdp/eql.mdp -c min2.gro -r min2.gro -p topol.top -o eql.tpr
+mpirun -np 1 -npernode 48 gmx_mpi mdrun -v -deffnm eql
+
+mpirun -np 1 -npernode 48 gmx_mpi grompp -f mdp/eql2.mdp -c eql.gro -r eql.gro -p topol.top -o eql2.tpr
+mpirun -np 1 -npernode 48 gmx_mpi mdrun -v -deffnm eql2
+
+mpirun -np 1 -npernode 48 gmx_mpi grompp -f mdp/prd.mdp -c eql2.gro -r eql2.gro -p topol.top -o prd.tpr
+mpirun -np 1 -npernode 48 gmx_mpi mdrun -v -deffnm prd
